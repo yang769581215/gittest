1f6238df77263ecad6eb77a5a404cfe4776df06d
frank.yang <frank.yang@pkfare.com>
2018-12-06 17:24:28 +0800

    update class user

--- java-git-test/src/main/java/com/frank/java/test/main/entity/Person.java
+++ java-git-test/src/main/java/com/frank/java/test/main/entity/Person.java
@@ -5,5 +5,28 @@ package com.frank.java.test.main.entity;
  * @Date 2018/12/6 17:14
  */
 public class Person {
+  private String pName;
+  private Integer age;
 
+  public Person(String pName, Integer age) {
+    this.pName = pName;
+    this.age = age;
+  }
+
+
+  public String getpName() {
+    return pName;
+  }
+
+  public void setpName(String pName) {
+    this.pName = pName;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
 }

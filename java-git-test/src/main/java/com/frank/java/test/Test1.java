fcae4e8592ea5e0d93a7e9543079c22eead5ba6a
frank.yang <frank.yang@pkfare.com>
2018-12-06 16:46:35 +0800

    commit test1

---
+++ java-git-test/src/main/java/com/frank/java/test/Test1.java
@@ -0,0 +1,12 @@
+package com.frank.java.test;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2018/12/6 16:45
+ */
+public class Test1 {
+
+  public static void main(String[] args) {
+    System.out.println("new test1");
+  }
+}

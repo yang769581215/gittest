fef14c2302aefdceb9289e4c02234a003023aa21
frank.yang <frank.yang@pkfare.com>
2018-12-06 16:31:30 +0800

    commit first java project

---
+++ java-git-test/src/main/java/com/frank/java/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.frank.java.test;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2018/12/6 16:30
+ */
+public class GitTest {
+
+  public static void main(String[] args) {
+    System.out.println("this is my git test project");
+  }
+
+}

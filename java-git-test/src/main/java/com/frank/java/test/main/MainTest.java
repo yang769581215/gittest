2305cdd6eb9fa87b373458c872cc828f5f92f743
frank.yang <frank.yang@pkfare.com>
2018-12-06 16:48:56 +0800

    new MainTest

---
+++ java-git-test/src/main/java/com/frank/java/test/main/MainTest.java
@@ -0,0 +1,17 @@
+package com.frank.java.test.main;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2018/12/6 16:48
+ */
+public class MainTest {
+  private String testName;
+
+  public String getTestName() {
+    return testName;
+  }
+
+  public void setTestName(String testName) {
+    this.testName = testName;
+  }
+}

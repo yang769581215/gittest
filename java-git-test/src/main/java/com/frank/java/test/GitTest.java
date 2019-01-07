24907bbf891362ce288923bf73bb24986008e529
frank.yang <frank.yang@pkfare.com>
2019-01-07 14:50:28 +0800

    Revert "add print test"
    This reverts commit a2162f4

--- java-git-test/src/main/java/com/frank/java/test/GitTest.java
+++ java-git-test/src/main/java/com/frank/java/test/GitTest.java
@@ -9,7 +9,6 @@ public class GitTest {
   public static void main(String[] args) {
     System.out.println("this is my git test project");
     System.out.println(" change my print ");
-    System.out.println("add test");
   }
 
 }

abe490701219095c16b1b57b8cd1ead342e116b9
frank.yang <frank.yang@pkfare.com>
2019-01-07 15:00:53 +0800

    add print address

--- java-git-test/src/main/java/com/frank/java/test/GitTest.java
+++ java-git-test/src/main/java/com/frank/java/test/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
   public static void main(String[] args) {
     System.out.println("this is my git test project");
     System.out.println(" change my print ");
+    System.out.println("add address");
   }
 
 }

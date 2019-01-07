6822e4c320b405140795a9cfc373660c50f6d210
frank.yang <frank.yang@pkfare.com>
2019-01-07 14:59:17 +0800

    add address

---
+++ java-git-test/src/main/java/com/frank/java/test/main/entity/Address.java
@@ -0,0 +1,17 @@
+package com.frank.java.test.main.entity;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2019/1/7 14:58
+ */
+public class Address {
+  private String addName;
+
+  public String getAddName() {
+    return addName;
+  }
+
+  public void setAddName(String addName) {
+    this.addName = addName;
+  }
+}

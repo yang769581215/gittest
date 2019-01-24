9087dc9d87f440ee92e7b1ddd54ce837d9990551
frank.yang <frank.yang@pkfare.com>
2019-01-24 11:48:42 +0800

    add:controller

---
+++ java-git-test/src/main/java/com/frank/java/test/main/entity/Company.java
@@ -0,0 +1,17 @@
+package com.frank.java.test.main.entity;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2019/1/12 14:49
+ */
+public class Company {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}

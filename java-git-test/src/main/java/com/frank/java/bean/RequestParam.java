e87f228983e2d75ea87f0574584800539383b5b8
frank.yang <frank.yang@pkfare.com>
2019-03-20 14:35:08 +0800

    add: param

---
+++ java-git-test/src/main/java/com/frank/java/bean/RequestParam.java
@@ -0,0 +1,26 @@
+package com.frank.java.bean;
+
+/**
+ * @Author Frank.Yang
+ * @Date 2019/3/20 14:34
+ */
+public class RequestParam {
+  private String requestKey;
+  private String sessionId;
+
+  public String getRequestKey() {
+    return requestKey;
+  }
+
+  public void setRequestKey(String requestKey) {
+    this.requestKey = requestKey;
+  }
+
+  public String getSessionId() {
+    return sessionId;
+  }
+
+  public void setSessionId(String sessionId) {
+    this.sessionId = sessionId;
+  }
+}
